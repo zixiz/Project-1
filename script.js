76806960e0b4ef27fc44435592074636d607141b
Shahar <gshachargilad@gmail.com>
2019-01-11 18:14:42 +0200

    Finished

--- script.js
+++ script.js
@@ -12,9 +12,26 @@ function createTask(task, date,hour) {
 function isTaskExiste(task){
     for(var i = 0; i < arr.length;i++){
         if(task == arr[i].task){
-            return arr[i].task
+            return true
         }
     }
+}
+// function backUp(){
+//     var backup = JSON.parse(localStorage.getItem("localstorageArr"))
+//     if(backup.length > 0){
+//         for(var i = 0;i<backUp.length;i++){
+//             var myContainer = document.getElementById("liContainer");
+//             var li = document.createElement("li");
+//             li.className = "listyle";
+//         li.innerHTML = "<i id='exitIcon' class='fas fa-times' onclick='removeNote(this)'></i>" +"<div class= 'taskContainer'><span>"+task+"</span></div>"+"<div class='dateAndTimeS'>"+"<span>"+date + "</span>"+ "<span>" + time +"</span></div>"
+//         myContainer.append(li);
+//         }
+//     }
+// }
+function clearForm(){
+    document.forms["inputForm"]["task"].value = "";
+    document.forms["inputForm"]["date"].value = "";
+    document.forms["inputForm"]["time"].value = "";
 }
 
 function addTask(){
@@ -22,14 +39,21 @@ function addTask(){
     var date = document.forms["inputForm"]["date"].value;
     var time = document.forms["inputForm"]["time"].value;
     
-    if(isTaskExiste(task) === task){
-        document.getElementById('error_msg').innerHTML = 'This task alredy exist';
+    
+    if(task === ""){
+        document.getElementById('error_msg').innerHTML = 'Must add a task';
+        clearForm()
+    }else if(date === ""){
+        document.getElementById('error_msg').innerHTML = 'Must add a date';
+    }else if(isTaskExiste(task) === true){
+        document.getElementById('error_msg').innerHTML = 'This task alredy exists';
     }else{
         arr.push(createTask(task,date,time));
+        // localStorage.setItem("localstorageArr", JSON.stringify(arr));
         var myContainer = document.getElementById("liContainer");
         var li = document.createElement("li");
         li.className = "listyle";
-        li.innerHTML = "<i id='exitIcon' class='fas fa-times' onclick='removeNote(this)'></i>" +"<div class= 'taskContainer'><span>"+task+"</span></div>"+"<div class='dateAndTimeS'>"+"<span>"+date + "</span>"+ "<span>" + time +"</span></div>"
+        li.innerHTML = "<i id='exitIcon' class='fas fa-times' onclick='removeNote(this)'></i>" +"<div class= 'taskContainer'><span>"+task+"</span></div>"+"<div class='dateAndTimeS'>"+"<span>"+date + "</span>"+"<br>"+ "<span>" + time +"</span></div>"
         myContainer.append(li);
         document.getElementById('error_msg').innerHTML = '';
 
@@ -43,7 +67,9 @@ function removeNote(x){
     for(var i =0; i<arr.length;i++){
         if(localTask == arr[i].task && localDate == arr[i].date){
             arr.splice(i, 1);
+            // localStorage.setItem("localstorageArr", JSON.stringify(arr));
         }
     }
     x.parentElement.parentElement.removeChild(x.parentElement);  
 }
+
